43dab066261b9ead7f429898da65c426f54a09f0
Kim <joakimbwire23@gmail.com>
2022-06-11 15:20:25 +0300

    updations on char.c

--- chars.c
+++ chars.c
@@ -1,5 +1,5 @@
-
-#include "main.h"
+/*all char realted functions*/
+#include "holberton.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -11,25 +11,25 @@
  */
 char *make_char(char *s, va_list vl)
 {
-  int i, j;
-  char *result;
+	int i, j;
+	char *result;
 
-  i = 0;
-  j = (*s == '%') ? 1 : 0;
-  while (*(s + j) != 'c' && _is_digit(*(s + j)))
-    {
-      i = 10 * i + *(s + j) - '0';
-      ++s;
-    }
-  if (i == 0)
-    i += 1;
-  result = malloc((i + 1) * sizeof(char));
+	i = 0;
+	j = (*s == '%') ? 1 : 0; /*if start with %*/
+	while (*(s + j) != 'c' && _is_digit(*(s + j)))
+	{
+		i = 10 * i + *(s + j) - '0';
+		++s;
+	}
+	if (i == 0)
+		i += 1;
+	result = malloc((i + 1) * sizeof(char));
 
-  for (j = 0; j < (i - 1); ++j)
-    result[j] = ' ';
-  result[j] = va_arg(vl, int);
-  result[i] = '\0';
-  return (result);
+	for (j = 0; j < (i - 1); ++j)
+		result[j] = ' ';
+	result[j] = va_arg(vl, int);
+	result[i] = '\0';
+	return (result);
 }
 
 
@@ -40,24 +40,24 @@ char *make_char(char *s, va_list vl)
  */
 int conversion_char(char *s)
 {
-  int i;
+	int i;
 
-  i = (*s == '%') ? 1 : 0;
-  if (*(s + i) == '0')
-    {
-      write(1, "no valid char format\n", 21);
-      exit(98);
-      return (0);
-    }
-  while (*(s + i) != 'c')
-    {
-      if (_is_digit(*(s + i)) == 0)
+	i = (*s == '%') ? 1 : 0;
+	if (*(s + i) == '0')
 	{
-	  write(1, "no valid char format\n", 21);
-	  exit(98);
-	  return (0);
+		write(1, "no valid char format\n", 21);
+		exit(98);
+		return (0);
 	}
-      ++i;
-    }
-  return (1);
+	while (*(s + i) != 'c')
+	{
+		if (_is_digit(*(s + i)) == 0)
+		{
+			write(1, "no valid char format\n", 21);
+			exit(98);
+			return (0);
+		}
+		++i;
+	}
+	return (1);
 }
